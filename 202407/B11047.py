bde54987e9f15ddd6fa62f27e39892e22f4ae0dc
yunjaeeun44 <yunmegan44@sookmyung.ac.kr>
2024-07-26 19:38:17 +0900

    20240726

---
+++ 202407/B11047.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+N, K = map(int, input().split())
+coins = []
+for _ in range(N):
+    coins.append(int(input()))
+
+answer = 0
+for i in range(N-1, -1, -1):
+    if coins[i] <= K:
+        answer += K // coins[i]
+        K = K % coins[i]
+
+print(answer)
